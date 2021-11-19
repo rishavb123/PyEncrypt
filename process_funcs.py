d838d85499f41189bec3d474741cf63cf4c67134
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-11-19 02:15:17 -0500

    rsa processors

--- process_funcs.py
+++ process_funcs.py
@@ -32,3 +32,48 @@ def character_representation(c: int) -> str:
         str: the character representation of the numeric representation
     """
     return chr(c + 97)
+
+def pad_numeric_representation(c: int) -> str:
+    """Pads a numeric representation to an even number of characters
+
+    Args:
+        c (int): the numeric representation to pad
+
+    Returns:
+        str: the padded numeric representation
+    """
+    s = str(c)
+    return s.zfill(len(s) + 1) if len(s) % 2 == 1 else s
+
+def convert_to_rsa_format(s: str) -> str:
+    """Converts a string to the RSA format
+
+    Args:
+        s (str): the string to convert
+
+    Returns:
+        str: the string in the RSA format
+    """
+    return ''.join(pad_numeric_representation(numeric_representation(c)) for c in s)
+
+def remove_spaces(s: str) -> str:
+    """Removes spaces from a string
+
+    Args:
+        s (str): the string to remove spaces from
+
+    Returns:
+        str: the string without spaces
+    """
+    return s.replace(' ', '')
+
+def convert_each_2_digits_to_char(s: str) -> str:
+    """Converts each two digits to a character
+
+    Args:
+        s (str): the string to convert
+
+    Returns:
+        str: the string converted to a character
+    """
+    return ''.join(character_representation(int(s[i:i+2])) for i in range(0, len(s), 2))
