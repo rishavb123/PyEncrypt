cef1cb4dce69ec274e7aca5dadf1b5dba5eda466
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-11-18 23:44:31 -0500

    added comments to processor functions

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -2,6 +2,14 @@ from encryption import Encryption
 
 # Preproccess Raw Text
 def to_lower_case(s: str) -> str:
+    """Converts string to lower case string
+
+    Args:
+        s (str): the string to convert to lower case
+
+    Returns:
+        str: the lower case string
+    """    
     return s.lower()
 
 
@@ -9,6 +17,14 @@ preprocess_raw_string = [to_lower_case]
 
 # Preprocess Characters
 def numeric_representation(c: str) -> int:
+    """Converts a character to a numeric representation
+
+    Args:
+        c (str): the character to convert to a numeric representation
+
+    Returns:
+        int: the numeric representation of the character
+    """    
     return ord(c) - 97
 
 
@@ -16,6 +32,14 @@ preprocess_groups = [numeric_representation]
 
 # Post Process Characters
 def character_representation(c: int) -> str:
+    """Converts a numeric representation to a character
+
+    Args:
+        c (int): the numeric representation to convert to a character
+
+    Returns:
+        str: the character representation of the numeric representation
+    """    
     return chr(c + 97)
 
 
